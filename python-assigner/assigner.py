ead7d7d72d04a5365f0897d5655b802e936bb186
Ben Smus <bensmus@gmail.com>
2024-11-25 14:00:36 -0800

    assigner works

--- python-assigner/assigner.py
+++ python-assigner/assigner.py
@@ -1,29 +1,64 @@
 import constraint as csp
+from typing import List
 
-rides = {
-    'helevator': {
-        'time': 10,
-        'workers': ['charlie', 'josh']
+problem_data = {
+    'rides': {
+        'rollercoaster': 22,
+        'helevator': 10,
+        'flume': 23,
+        'launcher': 23,
+        'carusel': 5
+    }, 
+    'workers': {
+        'alex': ['rollercoaster', 'launcher'],
+        'kennedy': ['flume', 'launcher'],
+        'gio': ['rollercoaster', 'helevator', 'carusel'],
+        'alexa': ['flume', 'helevator']
     },
-    'roller': {
-        'time': 20,
-        'workers': ['josh']
-    },
-    'carusel': {
-        'time': 5,
-        'workers': ['charlie']
-    },
-    'flume': {
-        'time': 30,
-        'workers': ['charlie', 'josh', 'terry']
-    }
+    'total_time': 23
 }
 
-def assigner(rides, total_time):
+def workers_that_can_check(ride, worker_permissions):
+    domain = []
+    for worker in worker_permissions:
+        if ride in worker_permissions[worker]:
+            domain.append(worker)
+    return domain
+
+rides = list(problem_data['rides'].keys())
+times = list(problem_data['rides'].values())
+ride_domains = [workers_that_can_check(ride, problem_data['workers']) for ride in rides]
+workers = list(problem_data['workers'].keys())
+total_time = problem_data['total_time']
+
+def assigner(
+        rides: List[str], 
+        times: List[int], 
+        ride_domains: List[List[str]], 
+        workers: List[str], 
+        total_time: int):
+    assert len(rides) == len(times) and len(rides) == len(ride_domains), 'invalid arguments'
     problem = csp.Problem(csp.MinConflictsSolver())
-    all_workers = set()
-    for ride in rides:
-        workers = rides[ride]['workers']
-        problem.addVariable(ride, workers)
-        all_workers.update(workers)
-    ...
+    for i, ride in enumerate(rides):
+        problem.addVariable(ride, ride_domains[i])
+    
+    def make_worker_constraint(target_worker: str):
+        # worker_constraint makes sure that the worker has enough
+        # time to check all of the rides.
+        def worker_constraint(*solution) -> bool:
+            # Solution is a list of workers, where the index tells you 
+            # which ride they check.
+            target_worker_time = 0
+            for i, worker in enumerate(solution):
+                if worker == target_worker:
+                    ride_time = times[i]
+                    target_worker_time += ride_time
+            return target_worker_time <= total_time
+        return worker_constraint
+    
+    for target_worker in workers:
+        problem.addConstraint(make_worker_constraint(target_worker), rides)
+
+    return problem.getSolution()
+
+print(assigner(rides, times, ride_domains, workers, total_time))
