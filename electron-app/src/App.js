532baf750889c94995531ec0494091196533377a
Ben Smus <bensmus@gmail.com>
2024-11-27 00:10:32 -0800

    applied day restriction

--- electron-app/src/App.js
+++ electron-app/src/App.js
@@ -2,25 +2,73 @@ import logo from './logo.svg';
 import './App.css';
 import { useEffect, useState, useRef } from 'react';
 
+const initialAppState = {
+    rides: {
+        rollercoaster: 22,
+        helevator: 10,
+        flume: 13,
+        launcher: 25
+    },
+    workers: { // JSON's keys aren't ordered, have to store array of ride names.
+        alex: ['rollercoaster', 'launcher'],
+        kennedy: ['flume', 'launcher'],
+        gio: ['rollercoaster', 'helevator'],
+        alexa: ['flume', 'helevator']
+    },
+    dayrestrict: {
+        monday: {
+            time: 25,
+            closedRides: ['rollercoaster', 'helevator'],
+            absentWorkers: ['alexa'],
+        },
+        tuesday: {
+            time: 50,
+            closedRides: [],
+            absentWorkers: ['kennedy'],
+        }
+    },
+    // ridechecks: Not user editable. It may be out of sync with dayrestrict
+    // (for example if the day name changes) until generate is hit again.
+    ridechecks: { 
+        thursday: {
+            'rollercoaster': 'alex',
+            'flume': 'alexa',
+            'launcher': 'gio',
+            'helevator': 'kennedy'
+        },
+        tuesday: {
+            'rollercoaster': 'alex',
+            'flume': 'alexa',
+            'launcher': 'gio',
+            'helevator': 'kennedy'
+        }
+    }
+};
+
+// Ridecheck generation is done for a certain day,
+// and we need to filter out closedRides and absentWorkers.
+function applyDayRestrict(appState, day) {
+    const {time, closedRides, absentWorkers} = appState['dayrestrict'][day]
+    const problemData = {
+        rides: Object.fromEntries(
+            // [ride, time] is being filtered.
+            Object.entries(appState.rides).filter(([ride, _]) => !closedRides.includes(ride))
+        ),
+        workers: Object.fromEntries(
+            Object.entries(appState.workers)
+                // [worker, workerRides] is being filtered.
+                .filter(([worker, _]) => !absentWorkers.includes(worker))
+                // Map workerRides such that it does not contain closed rides.
+                .map(([worker, workerRides]) => ([worker, workerRides.filter(ride => !closedRides.includes(ride))]))
+        ),
+        total_time: time
+    }
+    return problemData;
+}
+
 function App() {
     const [appState, setAppState] = useState('dummy state'); // Dummy state.
     const [apiResult, setApiResult] = useState('no composers'); // API not called yet.
-
-    const problem_data = {
-        rides: {
-            rollercoaster: 22,
-            helevator: 10,
-            flume: 13,
-            launcher: 25
-        },
-        workers: { // JSON's keys aren't ordered, have to store array of ride names.
-            alex: ['rollercoaster', 'launcher'],
-            kennedy: ['flume', 'launcher'],
-            gio: ['rollercoaster', 'helevator'],
-            alexa: ['flume', 'helevator']
-        },
-        total_time: 30
-    }
 
     // Set up an object whose .current always tracks appState.
     const appStateRef = useRef(appState);
@@ -80,13 +128,15 @@ function App() {
                 setApiResult('fetching composers...');
                 // Needed to set Access-Control-Allow-Origin to * in AWS console in order to make this work.
                 const url =  "https://grhg6g6d90.execute-api.us-west-2.amazonaws.com/ridecheck_generator";
+                const problemData = applyDayRestrict(initialAppState, 'monday');
+                console.log(problemData);
                 try {
                     const options = {
                         'method': 'POST',
                         'headers': {
                             'Content-Type': 'application/json',
                         },
-                        'body': JSON.stringify(problem_data)
+                        'body': JSON.stringify(problemData)
                     };
                     const response = await fetch(url, options);
                     const json = await response.json();
