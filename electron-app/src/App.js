118fc1a2c07b0752e90af7ca8d0f18e0a92001d5
Ben Smus <bensmus@gmail.com>
2024-12-08 14:12:08 -0800

    capitalized default app state

--- electron-app/src/App.js
+++ electron-app/src/App.js
@@ -6,55 +6,54 @@ import './App.css';
 
 const defaultAppState = {
     rides: [
-        {ride: 'rollercoaster', time: 22},
-        {ride: 'helevator', time: 10},
-        {ride: 'flume', time: 13},
-        {ride: 'launcher', time: 25}
+        {ride: 'Rollercoaster', time: 22},
+        {ride: 'Helevator', time: 10},
+        {ride: 'Flume', time: 13},
+        {ride: 'Launcher', time: 25}
     ],
     workers: [
-        {worker: 'alex', canCheck: ['rollercoaster', 'launcher']},
-        {worker: 'kennedy', canCheck: ['flume', 'launcher']},
-        {worker: 'gio', canCheck: ['rollercoaster', 'helevator']},
-        {worker: 'alexa', canCheck: ['flume', 'helevator']}
+        {worker: 'Alex', canCheck: ['Rollercoaster', 'Launcher']},
+        {worker: 'Kennedy', canCheck: ['Flume', 'Launcher']},
+        {worker: 'Gio', canCheck: ['Rollercoaster', 'Helevator']},
+        {worker: 'Alexa', canCheck: ['Flume', 'Helevator']}
     ],
     dayrestrict: [
         {
-            day: 'monday',
+            day: 'Monday',
             time: 25,
-            closedRides: ['rollercoaster', 'helevator'],
-            absentWorkers: ['alexa', 'kennedy'],
+            closedRides: ['Rollercoaster', 'Helevator'],
+            absentWorkers: ['Alexa', 'Kennedy'],
         },
         {
-            day: 'tuesday',
+            day: 'Tuesday',
             time: 50,
             closedRides: [],
-            absentWorkers: ['kennedy'],
+            absentWorkers: ['Kennedy'],
         }
     ],
     // ridechecks: Not user editable. It may be out of sync with dayrestrict
     // (for example if the day name changes) until generate is hit again.
     ridechecks: [
         {
-            day: 'thursday',
+            day: 'Thursday',
             ridecheck: {
-                'rollercoaster': 'alex',
-                'flume': 'alexa',
-                'launcher': 'gio',
-                'helevator': 'kennedy'
+                'Rollercoaster': 'Alex',
+                'Flume': 'Alexa',
+                'Launcher': 'Gio',
+                'Helevator': 'Kennedy'
             }
         },
         {
-            day: 'tuesday',
+            day: 'Tuesday',
             ridecheck: {
-                'rollercoaster': 'alex',
-                'flume': 'alexa',
-                'launcher': 'gio',
-                'helevator': 'kennedy'
+                'Rollercoaster': 'Alex',
+                'Flume': 'Alexa',
+                'Launcher': 'Gio',
+                'Helevator': 'Kennedy'
             }
         }
     ]
 };
-
 
 function getCsvString(headerArray, twoDimRowArray) {
     let csvString = headerArray.join(",") + "\n";
